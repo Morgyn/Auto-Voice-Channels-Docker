40269d26663d432be5910241dd74de6d3ce5bf7a
vinanrra <vicentefernandezguerra@gmail.com>
2020-07-12 12:54:46 +0200

    Added all config options
    Need to be verified

--- startAVC.sh
+++ startAVC.sh
@@ -15,6 +15,22 @@ if [ -z "${CLIENT_ID##*[!0-9]*}" ]; then
    exit -1
 fi
 
+if [ -z "${ADMIN_CHANNEL##*[!0-9]*}" ]; then
+   echo "ADMIN_CHANNEL must be a number"
+   exit -1
+fi
+
+if [ -z "${HEARTBEAT_TIMEOUT##*[!0-9]*}" ]; then
+   echo "HEARTBEAT_TIMEOUT must be a number"
+   exit -1
+fi
+
+if [ "$DISABLE_LOOP" == 'true' ] || [ "$DISABLE_LOOP" == 'false' ];
+then
+else
+   echo "DISABLE_LOOP must be true or false"
+   exit -1
+fi
 
 cd /AutoVoiceChannels
 
@@ -23,8 +39,13 @@ cat >config.json <<CONFIG_JSON
     "admin_id":$ADMIN_ID,
     "client_id":$CLIENT_ID,
     "log_timezone":"$TZ",
-    "token":"$TOKEN"
+    "token":"$TOKEN",
+    "disable_ready_message":$RDY_MESSAGE,
+    "admin_channel":$ADMIN_CHANNEL,
+    "disable_creation_loop":$DISABLE_LOOP,
+    "heartbeat_timeout":$HEARTBEAT_TIMEOUT
 }
+
 CONFIG_JSON
 
 python ./auto-voice-channels.py
